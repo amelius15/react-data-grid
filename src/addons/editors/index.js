98ebdacad7c0f8f117440b4f8ed833aef5abb163
amelius15 <anthonylobko@gmail.com>
2016-08-24 20:45:21 -0400

    added access to more editors

--- src/addons/editors/index.js
+++ src/addons/editors/index.js
@@ -2,7 +2,11 @@ const Editors = {
   AutoComplete: require('./AutoCompleteEditor'),
   DropDownEditor: require('./DropDownEditor'),
   SimpleTextEditor: require('./SimpleTextEditor'),
-  CheckboxEditor: require('./CheckboxEditor')
+  CheckboxEditor: require('./CheckboxEditor'),
+  AutoCompleteTokensEditor: require('./AutoCompleteTokensEditor'),
+  EditorBase: require('./EditorBase'),
+  EditorContainer: require('./EditorContainer'),
+  DateRangeEditor: require('./DateRangeEditor')
 };
 
 module.exports = Editors;
